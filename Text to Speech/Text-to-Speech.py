aa9b275682020f9b5eabeb9b970a704db74151b5
Pillai Ganesh Mohan <pillaiganeshmohan@gmail.com>
2021-05-30 21:44:52 +0530

    Update Text-to-Speech.py

--- Text to Speech/Text-to-Speech.py
+++ Text to Speech/Text-to-Speech.py
@@ -1,12 +1,6 @@
-## import libraries
-
 from tkinter import *
 from gtts import gTTS
 from playsound import playsound
-
-
-
-################### Initialized window####################
 
 root = Tk()
 root.geometry('350x300')
@@ -14,28 +8,15 @@ root.resizable(0,0)
 root.config(bg = 'ghost white')
 root.title('TEXT_TO_SPEECH')
 
-
-##heading
 Label(root, text = 'TEXT_TO_SPEECH' , font='arial 20 bold' , bg ='white smoke').pack()
 Label(root, text ='Ganesh' , font ='arial 15 bold', bg = 'white smoke').pack(side = BOTTOM)
 
-
-
-
-#label
 Label(root, text ='Enter Text', font ='arial 15 bold', bg ='white smoke').place(x=20,y=60)
 
-
-##text variable
 Msg = StringVar()
 
-
-#Entry
 entry_field = Entry(root,textvariable =Msg, width ='50')
 entry_field.place(x=20 , y=100)
-
-
-###################define function##############################
 
 def Text_to_speech():
     Message = entry_field.get()
@@ -49,11 +30,8 @@ def Exit():
 def Reset():
     Msg.set("")
 
-#Button
 Button(root, text = "PLAY" , font = 'arial 15 bold', command = Text_to_speech, width =4).place(x=25, y=140)
 Button(root,text = 'EXIT',font = 'arial 15 bold' , command = Exit, bg = 'OrangeRed1').place(x=100,y=140)
 Button(root, text = 'RESET', font='arial 15 bold', command = Reset).place(x=175 , y =140)
 
-
-#infinite loop to run program
 root.mainloop()
