8ddd15e7b920b5e8ca2b4a9a099e495edab3dc82
Pillai Ganesh Mohan <pillaiganeshmohan@gmail.com>
2021-05-30 20:48:50 +0530

    Update Rock-Paper-Scissor-Game.py

--- Rock Paper Scissor Game/Rock-Paper-Scissor-Game.py
+++ Rock Paper Scissor Game/Rock-Paper-Scissor-Game.py
@@ -1,39 +1,26 @@
-#import library
 from tkinter import *
 import random
 
-#initialize window
 root = Tk()
 root.geometry('400x400')
-root.resizable(0,0)
+root.resizable(0, 0)
 root.title('Rock,Paper,Scissors')
-root.config(bg ='seashell3')
+root.config(bg='seashell3')
 
+Label(root, text='Rock, Paper, Scissors', font='arial 20 bold', bg='seashell2').pack()
 
-#heading
-Label(root, text = 'Rock, Paper ,Scissors' , font='arial 20 bold', bg = 'seashell2').pack()
-
-
-##user choice
 user_take = StringVar()
-Label(root, text = 'choose any one: rock, paper ,scissors' , font='arial 15 bold', bg = 'seashell2').place(x = 20,y=70)
-Entry(root, font = 'arial 15', textvariable = user_take , bg = 'antiquewhite2').place(x=90 , y = 130)
+Label(root, text='choose any one: rock, paper, scissors', font='arial 15 bold', bg='seashell2').place(x=20, y=70)
+Entry(root, font='arial 15', textvariable=user_take, bg='antiquewhite2').place(x=90, y=130)
 
-
-
-#computer choice
-comp_pick = random.randint(1,3)
+comp_pick = random.randint(1, 3)
 if comp_pick == 1:
     comp_pick = 'rock'
-elif comp_pick ==2:
+elif comp_pick == 2:
     comp_pick = 'paper'
 else:
     comp_pick = 'scissors'
-    
 
-
-
-##function to play
 Result = StringVar()
 
 def play():
@@ -54,26 +41,20 @@ def play():
         Result.set('you win ,computer select paper')
     else:
         Result.set('invalid: choose any one -- rock, paper, scissors')
-    
-        
-    
-##fun to reset
+
 def Reset():
-    Result.set("") 
+    Result.set("")
     user_take.set("")
 
-##fun to exit
 def Exit():
     root.destroy()
 
+Entry(root, font='arial 10 bold', textvariable=Result, bg='antiquewhite2', width=50, ).place(x=25, y=250)
 
-###### button
-Entry(root, font = 'arial 10 bold', textvariable = Result, bg ='antiquewhite2',width = 50,).place(x=25, y = 250)
+Button(root, font='arial 13 bold', text='PLAY', padx=5, bg='seashell4', command=play).place(x=150, y=190)
 
-Button(root, font = 'arial 13 bold', text = 'PLAY'  ,padx =5,bg ='seashell4' ,command = play).place(x=150,y=190)
+Button(root, font='arial 13 bold', text='RESET', padx=5, bg='seashell4', command=Reset).place(x=70, y=310)
 
-Button(root, font = 'arial 13 bold', text = 'RESET'  ,padx =5,bg ='seashell4' ,command = Reset).place(x=70,y=310)
-
-Button(root, font = 'arial 13 bold', text = 'EXIT'  ,padx =5,bg ='seashell4' ,command = Exit).place(x=230,y=310)
+Button(root, font='arial 13 bold', text='EXIT', padx=5, bg='seashell4', command=Exit).place(x=230, y=310)
 
 root.mainloop()
